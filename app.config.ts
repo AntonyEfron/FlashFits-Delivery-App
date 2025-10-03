f3bbc17279cc454630b82a354e84a69f01ae900f
shubh4mb <shubhambiswas9899@gmail.com>
2025-10-03 18:12:42 +0530

    socket config

--- app.config.ts
+++ app.config.ts
@@ -3,7 +3,7 @@ export default ({ config }) => {
     return {
       ...config,
       extra: {
-        BACKEND_URL: "https://b65aea9fc66c.ngrok-free.app", // change to your backend URL
+        BACKEND_URL: "https://5eba71aa4387.ngrok-free.app", // change to your backend URL
       },
     };
   };
---
+++ config/socketConfig.js
@@ -0,0 +1,57 @@
+// src/config/riderSocket.ts
+import { io } from "socket.io-client";
+import mitt from "mitt";
+import Constants from "expo-constants";
+// import { registerRiderOrderListeners } from "./orderListeners";
+
+// Socket instance
+let socket = null;
+
+// Typed emitter for frontend components
+export const emitter = mitt();
+
+/**
+ * Connect rider socket when going online
+ * @param riderId string
+ */
+export const connectRiderSocket = (riderId) => {
+  const role = "deliveryRider";
+
+  socket = io(Constants.expoConfig.extra.BACKEND_URL, {
+    transports: ["websocket"],
+    query: { riderId, role },
+  });
+
+  socket.on("connect", () => {
+    console.log("✅ Rider connected to socket:", socket?.id);
+    socket?.emit("registerRider", { riderId }); // match backend
+// Backend can track online riders
+  });
+
+  socket.on("disconnect", () => {
+    console.log("❌ Rider disconnected from socket");
+  });
+
+  // Listen for order updates
+  registerRiderOrderListeners(socket);
+
+  return socket;
+};
+
+export const disconnectRiderSocket = () => {
+  if (socket) {
+    socket.disconnect();
+    socket = null;
+  }
+};
+
+
+export const registerRiderOrderListeners = (socket) => {
+  socket.on("newOrder", (order) => {
+    emitter.emit("newOrder", order);
+  });
+
+  socket.on("orderUpdate", (order) => {
+    emitter.emit("orderUpdate", order);
+  });
+};
