b7bf994be094c3c2e17bf23ce0d9361ea60c6abb
Antonyefron <antonyefron007@gmail.com>
2025-10-21 12:07:23 +0530

    lates stash

--- app.config.ts
+++ app.config.ts
@@ -3,8 +3,7 @@ export default ({ config }) => {
     return {
       ...config,
       extra: {
-        BACKEND_URL: "https://55a299101e7c.ngrok-free.app", // change to your backend URL
+        BACKEND_URL: "https://03430b25ab5a.ngrok-free.app", // change to your backend URL
       },
     };
   };
- 
