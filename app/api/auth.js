973589fac0876c08697deee4a0a0de00b9008de7
Antonyefron <antonyefron007@gmail.com>
2025-11-27 09:57:51 +0530

    78

--- app/api/auth.js
+++ app/api/auth.js
@@ -3,6 +3,7 @@
 
     export const verifyPhoneOtp = async (phone,otp) => {
         try {
+            console.log('87878787');
             const response = await axiosInstance.post("/deliveryRider/auth/verify-otp", {phone, otp });
             console.log(response.data);
             return response;
