0235a030793020994eed1379d5c6da8b07ed840d
shubh4mb <shubhambiswas9899@gmail.com>
2026-09-11 11:17:53 +0530

    sep 11

--- app.config.ts
+++ app.config.ts
@@ -5,7 +5,8 @@ export default ({ config }) => {
     extra: {
       ...config.extra,
       // BACKEND_URL:"https://662e0239d03e.ngrok-free.app", // change to your backend URL
-      BACKEND_URL: "https://api.theflashfits.com", // change to your backend URL
+      // BACKEND_URL: "https://api.theflashfits.com", // change to your backend URL
+      BACKEND_URL: "http://192.168.29.230:5000", // change to your backend URL
     },
   };
 };  
